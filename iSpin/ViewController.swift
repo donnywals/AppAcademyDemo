941ab032c91f7e88ed8f194171a051a51b59c40f
Donny Wals <donnywals@gmail.com>
2016-12-04 17:01:25 +0100

    implemented the basic UI

--- iSpin/ViewController.swift
+++ iSpin/ViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var enableNotificationsButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // check if notifications are enabled and call the relevant method if needed
     }
 
+    @IBAction func enableNotificationsTapped() {
+        // check if notifications are denied and open settings if needed
+    }
+    
+    func enableNotificationsAllowedUI() {
+        enableNotificationsButton.isHidden = true
+        
+        titleLabel.text = "Notifications are enabled, you'll receive a daily push message."
+    }
+    
+    func enableNotificationsDeniedUI() {
+        titleLabel.text = "You have disabled notifications for iSpin, go to your settings to enable notifications."
+    }
 }
 
