3300add51ea3846eaf76640317a3566904126bcf
Donny Wals <donnywals@gmail.com>
2016-12-04 20:55:56 +0100

    implemented the service extension

---
+++ iSpinWheelUI/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  iSpinWheelUI
+//
+//  Created by Donny Wals on 04/12/2016.
+//  Copyright © 2016 DonnyWals. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
