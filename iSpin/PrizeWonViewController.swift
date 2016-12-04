50a8a4e2882153b8bd680a64aa728713d75a19fe
Donny Wals <donnywals@gmail.com>
2016-12-04 17:08:24 +0100

    added win/lose view controllers

---
+++ iSpin/PrizeWonViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrizeWonViewController.swift
+//  iSpin
+//
+//  Created by Donny Wals on 04/12/2016.
+//  Copyright © 2016 DonnyWals. All rights reserved.
+//
+
+import UIKit
+
+class PrizeWonViewController: UIViewController {
+    
+    @IBAction func closeTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
